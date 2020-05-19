18eb4de65e0990fb717010a61118d588889e3c28
Max_M <56355528+GunSolo@users.noreply.github.com>
2020-05-19 20:40:40 +0300

    test build

---
+++ precache-manifest.bb5ae405557e6c09bc0966109f9e3183.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8158b46636ba77b9d1d",
+    "url": "/Diploma/static/css/main.a50f43e7.chunk.css"
+  },
+  {
+    "revision": "f8158b46636ba77b9d1d",
+    "url": "/Diploma/static/js/main.f8158b46.chunk.js"
+  },
+  {
+    "revision": "cee8c5f1911c0c8d851f",
+    "url": "/Diploma/static/js/runtime~main.cee8c5f1.js"
+  },
+  {
+    "revision": "6cc5d6831f6dfc2f37e6",
+    "url": "/Diploma/static/js/2.6cc5d683.chunk.js"
+  },
+  {
+    "revision": "5bd0df3dec62abef61a2eaeeaca7428c",
+    "url": "/Diploma/index.html"
+  }
+];
